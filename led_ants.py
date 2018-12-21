0be5f9724a89f8d05f11af7153ce514ca6060372
Andrew Scheller <github@loowis.durge.org>
2018-12-21 11:18:51 +0000

    Slightly neater code

--- led_ants.py
+++ led_ants.py
@@ -8,17 +8,13 @@ LEDS = (5,12,16,6,13,17)
 GPIO.setup(LEDS, GPIO.OUT, initial=0)
 
 try:
-        i = 0
+        prev = 0
         led_length = len(LEDS)
-        while i < led_length:
-                if i == 0:
-                        GPIO.output(LEDS[led_length-1],0)
-                else:
-                        GPIO.output(LEDS[i-1],0)
-                GPIO.output(LEDS[i],1)
-                i += 1
-                if i == led_length:
-                        i = 0
-                sleep(.2)
+        while True:
+                for i in range(led_length):
+                        GPIO.output(LEDS[prev],0)
+                        GPIO.output(LEDS[i],1)
+                        prev = i
+                        sleep(.2)
 except:
         GPIO.cleanup()
